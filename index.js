615a93d8fb2a434b87803ef9307bcbfb5e2ace0a
Victor Laitila <victorlaitila@MacBook-Pro-som-tillhor-Victor.local>
2025-01-20 20:45:46 +0200

    3.7, 3.8

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+morgan.token('post-data', function (req, res) { 
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+  return ' '
+})
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
 
 let persons = [
     { 
